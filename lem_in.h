35a89894dd71e125c5dc3c9e70c9a6ce2093f326
Adrien ROBION <arobion@e2r3p6.42.fr>
2018-01-23 13:03:50 +0100

    rangement du .h

--- lem_in.h
+++ lem_in.h
@@ -6,7 +6,7 @@
 /*   By: fbabin <fbabin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 18:05:23 by fbabin            #+#    #+#             */
-/*   Updated: 2018/01/23 12:51:24 by arobion          ###   ########.fr       */
+/*   Updated: 2018/01/23 13:03:15 by arobion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,21 +36,6 @@ typedef struct		s_room
 }					t_room;
 
 
-void				ft_lstndump(t_list **list);
-void				ft_lstnfree(t_list **t);
-char				**ft_verif_room_format(char *line);
-int					ft_verif_room_validity(char **room);
-int					ft_verif_room_is_uniq(char **room, t_list **t);
-int					ft_add_pipes_to_room(t_list **t, char *name1, char *name2);
-int					ft_are_rooms_exists(char *line, int i, t_list **t);
-void				freechar2(char **tab);
-int					get_pipes(char *line, t_list **t);
-int					ft_verif_line_is_comm(char *line);
-int					ft_launch_cmp(t_room *crawler, const char *name);
-void				ft_launch_pushback(t_room *room, char *name);
-long long			ft_atoi_check(const char *str);
-char				*get_rooms(char *line, t_list **t);
-t_room				*init_room(const char *str, int x, int y);
 /*
 ** ----------------------------------------------------------------------------
 ** ---------------------------------- SOURCES ---------------------------------
@@ -58,7 +43,31 @@ t_room				*init_room(const char *str, int x, int y);
 */
 
 /*
-** ------------------------------- SORT FUNCTIONS -----------------------------
+** ------------------------------- PARSE FUNCTIONS ----------------------------
 */
+
+char				*get_rooms(char *line, t_list **t);
+char				**ft_verif_room_format(char *line);
+int					ft_verif_room_validity(char **room);
+int					ft_verif_room_is_uniq(char **room, t_list **t);
+
+int					get_pipes(char *line, t_list **t);
+int					ft_add_pipes_to_room(t_list **t, char *name1, char *name2);
+int					ft_are_rooms_exists(char *line, int i, t_list **t);
+
+int					ft_verif_line_is_comm(char *line);
+long long			ft_atoi_check(const char *str);
+int					ft_launch_cmp(t_room *crawler, const char *name);
+void				ft_launch_pushback(t_room *room, char *name);
+t_room				*init_room(const char *str, int x, int y);
+void				ft_lstndump(t_list **list);
+
+/*
+** ------------------------------- FREE FUNCTIONS -----------------------------
+*/
+
+void				ft_lstnfree(t_list **t);
+void				freechar2(char **tab);
+
 
 #endif
