b30eef8bb586e768b23a87f8e7c85ed5cc519db6
Adrien ROBION <arobion@e2r3p4.42.fr>
2018-01-31 13:04:58 +0100

    oups

---
+++ freeer2.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   freeer2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arobion <arobion@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/31 12:33:23 by arobion           #+#    #+#             */
+/*   Updated: 2018/01/31 12:34:54 by arobion          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+void			free_listss(t_list *paths)
+{
+	t_list	*tmp;
+	t_list	*tmp2;
+
+	tmp = paths;
+	while (tmp->next)
+	{
+		tmp2 = tmp->next;
+		free(paths);
+		paths = tmp2;
+		tmp = tmp2;
+	}
+	free(paths);
+}
+
+void			freeit(t_list *path)
+{
+	t_list	*tmp;
+	t_list	*tmp2;
+
+	tmp = path;
+	while (tmp->next)
+	{
+		tmp2 = tmp->next;
+		free_listss(path->content);
+		free(path);
+		path = tmp2;
+		tmp = tmp2;
+	}
+	free_listss(path->content);
+	free(path);
+}
